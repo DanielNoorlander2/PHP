0842b47f5a3c598198b880d88d95dff9f06de907
DanielNoorlander2 <43062214+DanielNoorlander2@users.noreply.github.com>
2018-12-12 10:22:46 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 05/12/2018
+ * Time: 13:51
+ */
+$aantal = 14;
+$deze_jongen = "Deze jongen is ";
+
+echo $deze_jongen.$aantal. " jaar oud!";
